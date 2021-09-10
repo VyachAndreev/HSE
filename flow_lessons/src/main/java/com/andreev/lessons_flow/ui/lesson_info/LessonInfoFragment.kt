2db5f847f19e731436ff6afc739c160eef5d7da7
VyachAndreev <vyacheslavand@gmail.com>
2021-09-10 20:16:12 +0300

    hiding professor info

--- flow_lessons/src/main/java/com/andreev/lessons_flow/ui/lesson_info/LessonInfoFragment.kt
+++ flow_lessons/src/main/java/com/andreev/lessons_flow/ui/lesson_info/LessonInfoFragment.kt
@@ -1,14 +1,14 @@
 package com.andreev.lessons_flow.ui.lesson_info
 
 import android.os.Bundle
-import android.os.CpuUsageInfo
 import android.view.View
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.andreev.core.base.BaseFragment
 import com.andreev.core.di.ApplicationComponent
+import com.andreev.data.models.Lesson
 import com.andreev.lessons_flow.R
 import com.andreev.lessons_flow.databinding.FragmentLessonInfoBinding
-import com.andreev.lessons_flow.databinding.FragmentLessonsBinding
 import com.andreev.lessons_flow.ui.Constants
 import timber.log.Timber
 
@@ -30,16 +30,35 @@ class LessonInfoFragment: BaseFragment<FragmentLessonInfoBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.swipeLayout.isRefreshing = true
         binding.viewModel = viewModel
-        if (arguments == null)
-            Timber.i("Arguments are null")
         lessonId = arguments?.getString(Constants.lessonId, null)
         lessonId?.let { viewModel.getLesson(it) }
         viewModel.errorMessage.observe(viewLifecycleOwner, errorMessageObserver)
+        viewModel.lesson.observe(viewLifecycleOwner, lessonObserver)
+        binding.swipeLayout.setOnRefreshListener {
+            lessonId?.let { viewModel.getLesson(it) }
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(Constants.lessonId, lessonId)
+    }
+
+    private val lessonObserver = Observer<Lesson> {
+        if (binding.parentRelative.visibility == View.GONE) {
+            binding.parentRelative.visibility = View.VISIBLE
+        }
+        binding.swipeLayout.isRefreshing = false
+        if (viewModel.lesson.value?.lecturer == null) {
+            binding.professorNameTv.visibility = View.GONE
+            binding.professorTv.visibility = View.GONE
+            binding.professorView.visibility = View.GONE
+        } else {
+            binding.professorNameTv.visibility = View.VISIBLE
+            binding.professorTv.visibility = View.VISIBLE
+            binding.professorView.visibility = View.VISIBLE
+        }
     }
 }
