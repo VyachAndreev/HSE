637f20d55cfc2365507137dedffe92eac604b15a
VyachAndreev <vyacheslavand@gmail.com>
2021-09-10 18:34:49 +0300

    lesson call

--- core/src/main/java/com/andreev/core/api_interfaces/API.kt
+++ core/src/main/java/com/andreev/core/api_interfaces/API.kt
@@ -2,18 +2,31 @@ package com.andreev.core.api_interfaces
 
 import com.andreev.data.models.Lesson
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface API {
     @Throws(retrofit2.HttpException::class)
     @GET(lessonsUrl)
     suspend fun getLessons(
-        @Query("email") email: String = "vsandreev@edu.hse.ru",
-        @Query("start") startDate: String? = "2021-09-10",
-        @Query("end") endDate: String? = "2021-09-10"
+        @Query(email) eMail: String = testEmail,
+        @Query(start) startDate: String? = testStart,
+        @Query(end) endDate: String? = testEnd,
     ) : Array<Lesson>
 
+    @Throws(retrofit2.HttpException::class)
+    @GET(lessonUrl)
+    suspend fun getLesson(@Path(id) disciplineId: String?): Lesson
+
     private companion object {
+        const val email = "email"
+        const val testEmail = "vsandreev@edu.hse.ru"
+        const val start = "start"
+        const val testStart = "2021-09-10"
+        const val end = "end"
+        const val testEnd = "2021-09-10"
+        const val id = "id"
         const val lessonsUrl = "lessons"
+        const val lessonUrl = "lesson"
     }
 }
--- flow_lessons/src/main/java/com/andreev/lessons_flow/ui/lesson_info/LessonInfoViewModel.kt
+++ flow_lessons/src/main/java/com/andreev/lessons_flow/ui/lesson_info/LessonInfoViewModel.kt
@@ -1,6 +1,29 @@
 package com.andreev.lessons_flow.ui.lesson_info
 
+import androidx.lifecycle.MutableLiveData
 import com.andreev.core.base.BaseViewModel
+import com.andreev.data.models.Lesson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LessonInfoViewModel : BaseViewModel() {
+    val type = MutableLiveData<String>()
+    val discipline = MutableLiveData<String>()
+    val time = MutableLiveData<String>()
+    val online = MutableLiveData<String>()
+    val teacher = MutableLiveData<String>()
+    val teacherPosition = MutableLiveData<String>()
+    val link = MutableLiveData<String>()
+    val extraLink = MutableLiveData<String>()
+    val info = MutableLiveData<String>()
+    val lesson = MutableLiveData<Lesson>()
+
+    fun getLesson(id: String) {
+        scopeMain.launch {
+            lesson.value = withContext(Dispatchers.IO) {
+                api.getLesson(id)
+            }
+        }
+    }
 }
